68301495d03d69f00239ed3dd113a44569f20cee
Jonathan Heaney <jonathan.heaney@gmail.com>
2023-04-10 18:58:13 -0500

    Finish balance brackets studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,8 +9,62 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void oneOpenBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void oneClosedBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void bracketsWithStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[String]"));
+    }
+    @Test
+    public void bracketsWithStringFirstReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("String[]"));
+    }
+
+    @Test
+    public void doubleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
+    @Test
+    public void closedBracketFirstReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void closedBracketFirstInSecondSetReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[] ]["));
+    }
+
+    @Test
+    public void closedBracketFirstWithStringReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void balancedWithOtherCharactersReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[7%998/}{+]@#"));
+    }
+
+    @Test
+    public void manyBracketsLastMisnestedReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[ ]]]] []["));
+    }
+
+    @Test
+    public void lotsOfCorrectBracketsLastWrongReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[String] [String] [String] [String] [String] ]String["));
     }
 
 
